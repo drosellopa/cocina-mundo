b36aa2fd82cac639164638c439a6f4ea3e2fa02e
Daniel Roselló <danrosp@hotmail.com>
2025-03-22 16:56:40 +0100

    Sección recetas, galería y creación de footer, todo  en móvil

--- src/js/cocina.js
+++ src/js/cocina.js
@@ -1,3 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const mainMenu = document.querySelector('.main-menu');
+    const barsMenu = document.querySelector('.fa-bars');
+    const headerContainer = document.querySelector('.header-container');
+    const arrowUp = document.querySelector('.fa-arrow-up');
+    let menuOpen = false;
+    barsMenu.addEventListener('click', ()=>{        
+        menuOpen ? mainMenu.style.right =  '-100%' :  mainMenu.style.right = 0;
+        menuOpen = !menuOpen; 
+                
+    });
+    document.addEventListener('scroll', (e)=>{        
+        if(menuOpen){
+            mainMenu.style.right = '-100%';
+            menuOpen = false;
+        }
+        if(window.scrollY > 200){
+            headerContainer.style.backgroundColor = "#000d";
+            arrowUp.style.display = 'initial';
+        }else{
+            headerContainer.style.backgroundColor = "#0002";
+            arrowUp.style.display = 'none';
+        }
+        
+          
+    });
+
+
     
+    
+    
+ 
+
 });
